f1f254e0ea68b4b76b8923072fc666aeec862361
SebPan <49950453+SebPan@users.noreply.github.com>
2025-03-12 17:28:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ async def favicon():
 async def publish_message(data: MessageData):
     """Recibe un mensaje por API y lo publica en MQTT"""
     try:
-        result = mqtt_client.publish(TOPIC, data.message, qos=0, retain=False)
+        result = mqtt_client.publish(TOPIC, data.message, qos=0, retain=True)
         print("Mensaje")
         if result.rc != 0:
             raise HTTPException(status_code=500, detail=f"Error al publicar: {result.rc}")
